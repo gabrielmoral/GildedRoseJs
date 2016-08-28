f4bc1759c55b82997c7048161e822c7f966b342a
Gabriel Moral <gabrielmoralortiz@gmail.com>
2016-08-28 17:49:53 +0200

    Add builder to create test data

--- spec/gilded_rose.js
+++ spec/gilded_rose.js
@@ -15,9 +15,11 @@ describe('gilded_rose_should', function () {
 
   it('decrease_sell_in_and_quality_at_the_end_of_a_day', function () {
 
-    var quality, sell_in = 4;
-    var decreased_quality, decreased_sell_in = 3;
-    var item = new Item("Some", sell_in, quality);
+    var decreased_quality = 3;
+    var decreased_sell_in = 3;
+    var item = new ItemBuilder().with_quality(4)
+                                .with_sell_in(4)
+                                .build();
     
     Process([item]);
     
@@ -27,9 +29,9 @@ describe('gilded_rose_should', function () {
 
    it('quality_of_an_item_is_never_negative', function () {
      
-     var quality, sell_in = 0;
      var positive_numbers_edge = 0;
-     var item = new Item("Some", sell_in, quality);
+     var item = new ItemBuilder().with_quality(0)
+                                 .build();
 
      Process([item]);
 
@@ -37,11 +39,11 @@ describe('gilded_rose_should', function () {
   });
 
   it('quality_decrease_twice_when_sell_by_date_has_passed', function () {
-
-      var quality = 20;
-      var sell_in = 0;
+     
       var decreased_quality_twice_faster = 18;
-      var item = new Item("Some", sell_in, quality);
+      var item = new ItemBuilder().with_quality(20)
+                                  .with_sell_in(0)
+                                  .build();
 
       Process([item]);
 
@@ -50,14 +52,37 @@ describe('gilded_rose_should', function () {
 
    it('aged_brie_increases_in_quality_the_older_it_gets', function () {
 
-      var quality = 20;
-      var sell_in = 30;
       var increased_quality = 21;
-      var aged_brie = new Item("Aged Brie", sell_in, quality);
+      var aged_brie = new ItemBuilder().aged_brie()
+                                       .with_quality(20)
+                                       .with_sell_in(30)
+                                       .build();
 
       Process([aged_brie]);
 
       expect(aged_brie.quality).to.be.equals(increased_quality);
   });
-
 });
+
+function ItemBuilder(){
+  var item = new Item("Some item", 20, 20);
+
+  this.aged_brie = function(){
+    item.name = "Aged Brie";
+    return this;
+  }
+
+  this.with_quality = function(quality){
+    item.quality = quality;
+    return this;
+  }
+
+  this.with_sell_in = function(sell_in){
+    item.sell_in = sell_in;
+    return this;
+  }
+
+  this.build = function(){
+    return item;
+  }
+}
