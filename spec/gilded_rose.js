e41dbadea5326eda95e5338a3ceeed834dd2231f
Gabriel Moral <gabrielmoralortiz@gmail.com>
2016-08-28 18:39:07 +0200

    Add more tests to prepare the solution before refactoring

--- spec/gilded_rose.js
+++ spec/gilded_rose.js
@@ -62,6 +62,74 @@ describe('gilded_rose_should', function () {
 
       expect(aged_brie.quality).to.be.equals(increased_quality);
   });
+
+     it('the_quality_of_an_item_is_never_more_than_50', function () {
+
+      var quality = 50;
+      var aged_brie = new ItemBuilder().aged_brie()
+                                       .with_quality(50)
+                                       .with_sell_in(30)
+                                       .build();
+
+      Process([aged_brie]);
+
+      expect(aged_brie.quality).to.be.equals(quality);
+  });
+
+  it('legendary_items_are_not_sold_or_decreased_quality', function () {
+
+      var quality = 50;
+      var sell_in = 30;
+
+      var sulfuras = new ItemBuilder().sulfuras()
+                                       .with_quality(50)
+                                       .with_sell_in(30)
+                                       .build();
+
+      Process([sulfuras]);
+
+      expect(sulfuras.quality).to.be.equals(quality);
+      expect(sulfuras.sell_in).to.be.equals(sell_in);
+  });
+
+  it('backstage_passes_increase_quality_by_2_when_there_are_10_or_less_sell_in_days', function () {
+
+      var increased_quality = 22;
+      var backstage_pass = new ItemBuilder().backstage_passes()
+                                       .with_quality(20)
+                                       .with_sell_in(10)
+                                       .build();
+
+      Process([backstage_pass]);
+
+      expect(backstage_pass.quality).to.be.equals(increased_quality);
+  });
+
+  it('backstage_passes_increase_quality_by_3_when_there_are_5_or_less_sell_in_days', function () {
+
+      var increased_quality = 23;
+      var backstage_pass = new ItemBuilder().backstage_passes()
+                                       .with_quality(20)
+                                       .with_sell_in(5)
+                                       .build();
+
+      Process([backstage_pass]);
+
+      expect(backstage_pass.quality).to.be.equals(increased_quality);
+  });
+
+  it('backstage_passes_drops_quality_to_0_after_the_concert', function () {
+
+      var quality = 0;
+      var backstage_pass = new ItemBuilder().backstage_passes()
+                                       .with_quality(20)
+                                       .with_sell_in(0)
+                                       .build();
+
+      Process([backstage_pass]);
+
+      expect(backstage_pass.quality).to.be.equals(quality);
+  });
 });
 
 function ItemBuilder(){
@@ -69,6 +137,16 @@ function ItemBuilder(){
 
   this.aged_brie = function(){
     item.name = "Aged Brie";
+    return this;
+  }
+
+  this.backstage_passes = function(){
+    item.name = "Backstage passes to a TAFKAL80ETC concert";
+    return this;
+  }
+
+  this.sulfuras = function(){
+    item.name = "Sulfuras, Hand of Ragnaros";
     return this;
   }
 
