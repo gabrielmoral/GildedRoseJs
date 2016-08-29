687546b773d2e3f1c2da46e280e6311c81dc2d33
Gabriel Moral <gabrielmoralortiz@gmail.com>
2016-08-29 21:01:09 +0200

    Unify if branches

--- gilded_rose.js
+++ gilded_rose.js
@@ -2,8 +2,8 @@
 'use strict';
 
 module.exports = {
-  Item : Item, 
-  Process : update_quality
+  Item: Item,
+  Process: update_quality
 };
 
 
@@ -15,31 +15,23 @@ function Item(name, sell_in, quality) {
 
 
 function update_quality(items) {
-  
+
   var aged_brie = 'Aged Brie';
   var backstage_passes = 'Backstage passes to a TAFKAL80ETC concert';
   var sulfuras = 'Sulfuras, Hand of Ragnaros';
-  
+
   for (var i = 0; i < items.length; i++) {
     if (items[i].name != aged_brie && items[i].name != backstage_passes) {
-      if (items[i].quality > 0) {
-        if (items[i].name != sulfuras) {
-          items[i].quality = items[i].quality - 1
-        }
+      if (items[i].quality > 0 && items[i].name != sulfuras) {
+        items[i].quality = items[i].quality - 1
       }
     } else {
       if (items[i].quality < 50) {
         items[i].quality = items[i].quality + 1
-        if (items[i].name == backstage_passes) {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
+        if (items[i].name == backstage_passes && items[i].sell_in < 11 && items[i].quality < 50) {
+          items[i].quality = items[i].quality + 1
+          if (items[i].sell_in < 6 && items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1
           }
         }
       }
@@ -50,10 +42,8 @@ function update_quality(items) {
     if (items[i].sell_in < 0) {
       if (items[i].name != aged_brie) {
         if (items[i].name != backstage_passes) {
-          if (items[i].quality > 0) {
-            if (items[i].name != sulfuras) {
+          if (items[i].quality > 0 && items[i].name != sulfuras) {
               items[i].quality = items[i].quality - 1
-            }
           }
         } else {
           items[i].quality = items[i].quality - items[i].quality
