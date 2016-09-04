67115b8b7bcfb325935da82fa5a5fde63096f496
Gabriel Moral <gabrielmoralortiz@gmail.com>
2016-09-04 14:03:15 +0200

    Extract method to map items and refactor update quality method

--- gilded_rose.js
+++ gilded_rose.js
@@ -28,7 +28,7 @@ var itemPrototype = {
     return this.item.quality === max_item_quality;
   },
 
-  sell_in_lower_than : function(days) {
+  sell_in_lower_than: function (days) {
     return this.item.sell_in < days;
   }
 }
@@ -36,7 +36,7 @@ var itemPrototype = {
 function AgedBrie(item) {
   this.item = item;
 
-  AgedBrie.prototype.update_quality = () => {
+  this.update_quality = () => {
     this.increase_quality();
     this.decrease_sell_in(item);
   }
@@ -45,34 +45,31 @@ function AgedBrie(item) {
 function RegularItem(item) {
   this.item = item;
 
-  RegularItem.prototype.update_quality = () => {
-    if (has_some_quality()) {
-      decrease_quality();
-    }
+  this.update_quality = () => {
+    decrease_quality();
 
     this.decrease_sell_in();
 
     if (this.sell_in_lower_than(0)) {
-      if (has_some_quality()) {
-        decrease_quality();
-      }
+      decrease_quality();
     }
   }
 
-  function has_some_quality() {
-    var minimum_item_quality = 0;
-    return item.quality > minimum_item_quality;
+  function decrease_quality() {
+    if (!has_quality()) return;
+    item.quality = item.quality - 1;
   }
 
-  function decrease_quality() {
-    item.quality = item.quality - 1;
+  function has_quality() {
+    var minimum_item_quality = 0;
+    return item.quality > minimum_item_quality;
   }
 }
 
 function BackstagePass(item) {
   this.item = item;
 
-  BackstagePass.prototype.update_quality = () => {
+  this.update_quality = () => {
 
     if (this.sell_in_lower_than(11)) {
       this.increase_quality();
@@ -100,21 +97,23 @@ RegularItem.prototype = itemPrototype;
 
 function update_quality(items) {
 
+  items.map(map_item)
+    .forEach(item => item.update_quality());
+}
+
+function map_item(item) {
+
+  var sulfuras = 'Sulfuras, Hand of Ragnaros';
   var aged_brie = 'Aged Brie';
   var backstage_passes = 'Backstage passes to a TAFKAL80ETC concert';
-  var sulfuras = 'Sulfuras, Hand of Ragnaros';
 
-  var items_to_be_processed = items.filter(item => item.name !== sulfuras);
-
-  items_to_be_processed.filter(item => item.name == backstage_passes)
-    .map(item => new BackstagePass(item))
-    .forEach(backstage_pass => backstage_pass.update_quality());
-
-  items_to_be_processed.filter(item => item.name == aged_brie)
-    .map(item => new AgedBrie(item))
-    .forEach(aged_brie => aged_brie.update_quality());
-
-  items_to_be_processed.filter(item => item.name != aged_brie && item.name != backstage_passes)
-    .map(item => new RegularItem(item))
-    .forEach(regular_item => regular_item.update_quality());
+  if (item.name == backstage_passes) {
+    return new BackstagePass(item);
+  } else if (item.name == aged_brie) {
+    return new AgedBrie(item);
+  } else if (item.name == sulfuras){
+    return { update_quality : () => {}}
+  } else {
+    return new RegularItem(item);
+  }
 }
